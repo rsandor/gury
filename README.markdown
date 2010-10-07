0178d246a4478618be5282149a5f40a833ef9f72
Ryan Sandor Richards <sandor.richards@gmail.com>
2010-10-07 04:09:12 -0700

    First Commit

---
+++ README.markdown
@@ -0,0 +1,45 @@
+gury - A jQuery inspired canvas utility library
+================================================================================
+by Ryan Sandor Richards
+
+Gury (pronounced "jury") is a simple to use utility library for drawing and
+animating when using HTML5 canvas tags. It takes the guess work out of setting
+and allows you to build your scenes one object at a time.
+
+It was built with simplicity in mind and its usage was modeled in the image
+of jQuery. For instance you can initialize, style, and animate an entire scene
+in a single expression using chaining.
+
+Usage
+--------------------------------------------------------------------------------
+
+Getting started with Gury is a piece of cake! Here's a step-by-step guide:
+
+1) Create an HTML page and add a canvas with an id attribute to the body
+2) Include `gury.js` in a script tag
+3) Write some JavaScript to make a simple animation
+
+To see it in action view the `demo.html` file in a canvas enabled browser.
+
+
+License (MIT)
+--------------------------------------------------------------------------------
+Copyright (c) 2010 Ryan Sandor Richards
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ gury.js
@@ -0,0 +1,111 @@
+/*
+  gury.js - A jQuery inspired canvas utility library
+
+  Copyright (c) 2010 Ryan Sandor Richards
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
+*/
+window.$g = (function() {
+  /*
+   * Core Gury Class
+   */
+  
+  function Gury(canvas) {
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
+    this._objects = [];
+    this._paused = false;
+    this._loop_interval = null;
+  }
+  
+  /*
+   * Canvas style methods
+   */
+  
+  Gury.prototype.size = function(w, h) {
+    this.canvas.width = w;
+    this.canvas.height = h;
+    return this;
+  };
+  
+  Gury.prototype.background = function(bg) {
+    this.canvas.style.background = bg;
+    return this;
+  };
+  
+  /*
+   * Objects and Rendering
+   */
+  
+  Gury.prototype.add = function(d) {
+    this._objects.push(d);
+    return this;
+  };
+  
+  Gury.prototype.draw = function() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    for (var i = 0; i < this._objects.length; i++) {
+      var ob = this._objects[i];
+      if (typeof ob == "function") {
+        ob.call(this, this.ctx);
+      }
+      else if (typeof ob == "object" && typeof ob.draw != "undefined") {
+        ob.draw(this.ctx);
+      }
+    }
+    return this;
+  };
+  
+  /*
+   * Animation Controls
+   */
+  
+  Gury.prototype.play = function(interval) {
+    var _gury = this;
+    this._loop_interval = setInterval(function() {
+      if (!_gury._paused) {
+        _gury.draw();
+      }
+    }, interval);
+    return this;
+  };
+  
+  Gury.prototype.pause = function() {
+    this._paused = !this._paused;
+    return this;
+  };
+  
+  Gury.prototype.stop = function() {
+    if (this._loop_interval != null) {
+      clearInterval(this._loop_interval);
+      this._paused = false;
+    }
+    return this;
+  };
+  
+  /*
+   * Public interface
+   */
+  
+  function GuryInterface(id) {
+    return new Gury(document.getElementById(id));
+  }
+  
+  return GuryInterface;
+})();
